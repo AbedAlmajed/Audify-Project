324d3c8bb43199e66f64c962fefe1e07f7caa40f
AbedAlmajed <abedalmajedalajarmah@gmail.com>
2024-07-03 21:38:05 +0300

    update navbar in details page

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,6 +1,4 @@
-// //  step number 2
-// Import the functions you need from the SDKs you need
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
   getAuth,
@@ -12,8 +10,6 @@ import {
   setDoc,
   doc,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
